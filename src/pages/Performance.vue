232455b3e1afa448387f62bfbbd52f893a97af3a
yanln <yanln_2017@126.com>
2018-06-22 17:32:15 +0800

    update 2018-6-22

--- src/pages/Performance.vue
+++ src/pages/Performance.vue
@@ -3,14 +3,15 @@
       <top title="实习表现" :showBack="true"/>
       <r-body>
            <r-card title="实习评价">
-                  <r-input title="分数:" :readonly="true"  :max="100" :min="0"  :model="this" value="v_score" :isNumber="true"/>
-                  <r-textarea title="评价:" :readonly="true" :model="this" value="comments" :height="300" :max="600"></r-textarea>
+                  <r-input title="实习出勤分:" :readonly="true"  :max="100" :min="0"  :model="this" value="v_score_1" :isNumber="true"/>
+                  <r-input title="实习材料分:" :readonly="true"  :max="100" :min="0"  :model="this" value="v_score_2" :isNumber="true"/>
+                  <r-textarea title="评价:" :readonly="true" :model="this" value="comments" :height="150" :max="600"></r-textarea>
             </r-card>
-            <r-card title="出勤记录表现">
+           <!--  <r-card title="出勤记录表现">
                   <div id="record" style="width: 100%;height:270px;"></div>
-            </r-card>
+            </r-card> -->
             <r-card title="实习表现">
-                  <div id="perform" style="width: 100%;height:270px;"></div>
+                  <div id="perform" style="width: 100%;height:265px;"></div>
             </r-card>
       </r-body>
            
@@ -23,14 +24,17 @@ export default {
   data(){
       return {
           comments:null,
-          v_score:null
+          v_score_1:null,
+          v_score_2:null,
+          v_studentNo:null,
+          performInfo: {}
       }
   },
   async mounted (){
-        const myChart = echarts.init(document.getElementById('record'));
+       /*  const myChart = echarts.init(document.getElementById('record'));
         const option = {
             title: {
-                text: '实习表现'
+                text: '出勤记录表现'
             },
             tooltip: {},
             legend: {
@@ -46,62 +50,81 @@ export default {
                 data: [5, 20, 36, 10]
             }]
         };
-        myChart.setOption(option);
-
-        const perform = echarts.init(document.getElementById('perform'));
-        const performOption = {
-            tooltip: {
-                trigger: 'item',
-                formatter: "{a} <br/>{b}: {c} ({d}%)"
-            },
-            legend: {
-                orient: 'vertical',
-                x: 'left',
-                data:['优秀','良好','一般','较差','差']
-            },
-            series: [
-                {
-                    name:'记录表现',
-                    type:'pie',
-                    radius: ['50%', '70%'],
-                    avoidLabelOverlap: false,
-                    label: {
-                        normal: {
-                            show: false,
-                            position: 'center'
-                        },
-                        emphasis: {
-                            show: true,
-                            textStyle: {
-                                fontSize: '30',
-                                fontWeight: 'bold'
-                            }
-                        }
-                    },
-                    labelLine: {
-                        normal: {
-                            show: false
-                        }
-                    },
-                    data:[
-                        {value:335, name:'优秀'},
-                        {value:310, name:'良好'},
-                        {value:234, name:'一般'},
-                        {value:135, name:'较差'},
-                        {value:1548, name:'差'}
-                    ]
-                }
-            ]
-       };
-       
-       perform.setOption(performOption);
+        myChart.setOption(option); */
 
         const url = "intern/attendenceAppraisal/stu";
         const temp_record = await this.$http.get(url);
         if(temp_record.body){
             this.comments = temp_record.body.comments;
-            this.v_score = temp_record.body.score;
+            this.v_score_1 = temp_record.body.attendanceScore;
+            this.v_score_2 = temp_record.body.documentScore;
+            this.v_studentNo = temp_record.body.studentNo;
         }
+
+        // 获取生成出勤饼图的数据
+        if(this.v_studentNo !== 'null'){
+                const url = "intern/summary/student/attendence?studentNo=" + this.v_studentNo;
+                const temp_attend = await this.$http.get(url);
+                if(temp_attend.body){
+                    const performInfoTemp = {};
+                    performInfoTemp['attendaceDays']= temp_attend.body.attendaceDays;
+                    performInfoTemp['sickLeaveDays'] = temp_attend.body.sickLeaveDays;
+                    performInfoTemp['personalDays'] = temp_attend.body.personalDays;
+                    performInfoTemp['absenteeismDays'] = temp_attend.body.absenteeismDays;
+
+                    this.performInfo = performInfoTemp;
+                }
+        }
+
+        // 开始生成出勤饼图
+        const perform = echarts.init(document.getElementById('perform'));
+        const performOption = {
+              tooltip: {
+                  trigger: 'item',
+                  formatter: "{a} <br/>{b}: {c} ({d}%)"
+              },
+              legend: {
+                  orient: 'vertical',
+                  x: 'left',
+                  data:['出勤天数','病假天数','事假天数','旷工天数']
+              },
+              series: [
+                  {
+                      name:'实习表现',
+                      type:'pie',
+                      radius: ['50%', '70%'],
+                      avoidLabelOverlap: false,
+                      label: {
+                          normal: {
+                              show: false,
+                              position: 'center'
+                          },
+                          emphasis: {
+                              show: true,
+                              textStyle: {
+                                  fontSize: '30',
+                                  fontWeight: 'bold'
+                              }
+                          }
+                      },
+                      labelLine: {
+                          normal: {
+                              show: false
+                          }
+                      },
+                      data:[
+                          {value:this.performInfo.attendaceDays, name:'出勤天数'},
+                          {value:this.performInfo.sickLeaveDays, name:'病假天数'},
+                          {value:this.performInfo.personalDays, name:'事假天数'},
+                          {value:this.performInfo.absenteeismDays, name:'旷工天数'}
+                      ]
+                  }
+              ]
+        };
+
+        perform.setOption(performOption);
+
+        
   }
  
 };
--- src/pages/PerformanceDetail.vue
+++ src/pages/PerformanceDetail.vue
@@ -1,18 +1,21 @@
 <template>
   <r-page>
       <top title="实习评价详情" :showBack="true"/>
-      <r-body>
+        <r-body>
+
+            <r-card title="实习表现：">
+                <div id="daysStr" ></div>
+                <div id="perform" style="width: 100%; height:265px;"></div>
+            </r-card>
+            
             <r-card title="实习评价:">
                 <r-input title="实习出勤分"   placeholder="最高15分"  :max="100" :min="0"  :model="this" value="v_score_1" :isNumber="true"/>
                 <r-input title="实习材料提交分"   placeholder="最高15分"  :max="100" :min="0"  :model="this" value="v_score_2" :isNumber="true"/>
                 <r-textarea placeholder="请输入评价"  :model="this" value="comments" :height="200" :max="600"></r-textarea>
             </r-card>
+            
 
-              <r-card title="实习表现:">
-                  <div id="perform" style="width: 100%;height:270px;"></div>
-            </r-card>
-
-      </r-body>
+        </r-body>
              <r-tab-bar>
                   <r-cell type="row" :vertical="true">
                                 <r-cell v-if="!score || status !== 1">
@@ -42,7 +45,8 @@ export default {
       v_score_1: null,
       v_score_2: null,
       status: null,
-      performInfo: {}
+      performInfo: {},
+      daysStr:null
     };
   },
   methods: {
@@ -110,9 +114,10 @@ export default {
                   }
           }
 
+       // 获取生成出勤饼图的数据
           const studentNo = this.$route.query.studentNo;
           if(studentNo !== 'null'){
-                  const url = "intern/summary/student/attendence?studentNo="+studentNo;
+                  const url = "intern/summary/student/attendence?studentNo=" + studentNo;
                   const temp_attend = await this.$http.get(url);
                   if(temp_attend.body){
                     const performInfoTemp = {};
@@ -122,9 +127,100 @@ export default {
                     performInfoTemp['absenteeismDays'] = temp_attend.body.absenteeismDays;
 
                     this.performInfo = performInfoTemp;
+
+                    let dayStr1 = '出勤天数: ' + temp_attend.body.attendaceDays + '天，病假天数' +  temp_attend.body.sickLeaveDays + '天';
+                    let dayStr2 = '<br/>事假天数' + temp_attend.body.personalDays + '天，旷工天数' + temp_attend.body.absenteeismDays + '天';
+                    document.getElementById('daysStr').innerHTML =  dayStr1 + dayStr2;
                   }
           }
 
+
+            
+          // 开始生成出勤饼图
+          const perform = echarts.init(document.getElementById('perform'));
+          const performOption = {
+                title : {  
+                    text: '实习表现',  
+                    subtext: '',  
+                    x:'left',  
+                    textStyle:{  
+                        color:"#222",                             
+                        fontStyle:"normal",                      
+                        fontWeight:"600",  
+                        fontFamily:"san-serif",  
+                        fontSize:16,     
+                    }  
+                },  
+                tooltip : {  
+                    trigger: 'item',                        
+                  formatter: "{a} {b} : ({c}天) {d}%"  
+                },  
+                legend: {  
+                    x : '70%',  
+                        y : '25%',  
+                    orient: 'vertical',  
+                    left: 'left',  
+                    itemWidth:10,  
+                    itemHeight:10,  
+                    selectedMode:false, //禁止点击  
+                    textStyle: {    
+                                fontSize: 12 ,  
+                                color:"#999",    
+                             },   
+                    formatter: function (name) {  
+                                   return name.length > 4 ? (name.slice(0,4)+"...") : name                                        
+                        },  
+                    data:['出勤天数','病假天数','事假天数','旷工天数']
+                },  
+                series : [  
+                    {  
+                        name: '',  
+                        type: 'pie',  
+                        radius : '50%',  // '75%'饼状   ['40%', '70%']环状
+                        center: ['50%', '40%'],  
+                        hoverAnimation:false, //是否开启 hover 在扇区上的放大动画效果  
+                        selectedMode:'single',  //选中模式，表示是否支持多个选中，默认关闭，支持布尔值和字符串，字符串取值可选'single'，'multiple'，分别表示单选还是多选。  
+                        selectedOffset:5, //选中扇区的偏移距离  
+                        data:[
+                            {value:this.performInfo.attendaceDays, name:'出勤天数'},
+                            {value:this.performInfo.sickLeaveDays, name:'病假天数'},
+                            {value:this.performInfo.personalDays, name:'事假天数'},
+                            {value:this.performInfo.absenteeismDays, name:'旷工天数'}
+                        ],
+                        itemStyle: {  
+                            normal:{  
+                                label:{  
+                                    show:true,  
+                                   formatter: function(params){  
+                                           var name=params.name; //名字  
+                                           var percent=params.percent; //占比  
+                                           var value=params.value; //数量  
+                                           if(name.length>8){  
+                                                return name.slice(0,7)+"..."+"\n"+"("+value+"天)"+percent+"%";  
+                                           }else{  
+                                                return name+"\n"+"("+value+"天)"+percent+"%";  
+                                           }  
+                                   }                                     
+                                  },  
+                                 labelLine:{  
+                                    show:true  
+                                  }  
+                                },  
+                            emphasis: {  
+                                shadowBlur: 10,  
+                                shadowOffsetX: 0,  
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'  
+                            }  
+                        }  
+                    }  
+                ],  
+
+               color: ['rgb(187,140,238)','rgb(134,146,243)','rgb(60,184,255)','rgb(113,171,246)','rgb(255,193,134)']  
+            };
+            perform.setOption(performOption);
+
+          /*   
+            // 开始生成出勤饼图
           const perform = echarts.init(document.getElementById('perform'));
           const performOption = {
               tooltip: {
@@ -141,7 +237,7 @@ export default {
                       name:'实习表现',
                       type:'pie',
                       radius: ['50%', '70%'],
-                      avoidLabelOverlap: false,
+                      avoidLabelOverlap: true,
                       label: {
                           normal: {
                               show: false,
@@ -161,15 +257,16 @@ export default {
                           }
                       },
                       data:[
-                          {value:this.performInfo.attendaceDays, name:'出勤天数'},
-                          {value:this.performInfo.sickLeaveDays, name:'病假天数'},
-                          {value:this.performInfo.personalDays, name:'事假天数'},
-                          {value:this.performInfo.absenteeismDays, name:'旷工天数'}
+                          {value:this.performInfo.attendaceDays, name:'出勤天数[ ' + (!this.performInfo.attendaceDays ? 0 : this.performInfo.attendaceDays) + ' 天 ]'},
+                          {value:this.performInfo.sickLeaveDays, name:'病假天数[ '+ (!this.performInfo.sickLeaveDays ? 0 : this.performInfo.sickLeaveDays)+ ' 天 ]'},
+                          {value:this.performInfo.personalDays, name:'事假天数[ ' + (!this.performInfo.personalDays ? 0 : this.performInfo.personalDays) + ' 天 ]'},
+                          {value:this.performInfo.absenteeismDays, name:'旷工天数[ ' + (!this.performInfo.absenteeismDays ? 0 : this.performInfo.absenteeismDays) + ' 天 ]'}
                       ]
                   }
               ]
           };
-          perform.setOption(performOption);
+          perform.setOption(performOption); */
+
 
   
   }
