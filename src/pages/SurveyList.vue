9e6c42c1b2b84b43abb6478e27b9dba6200d74c8
yanln <yanln_2017@126.com>
2018-07-30 13:27:32 +0800

    update 2018-7-30

--- src/pages/SurveyList.vue
+++ src/pages/SurveyList.vue
@@ -3,9 +3,9 @@
       <top title="走访记录列表" :showBack="true"/>
       <r-body>
               <search :condition="condition" :callBack="search"  />
-              <r-card>
+              <!-- <r-card>
                   <r-selector  title="状态" :options="options" :model="this" value="status" :onChange="search"></r-selector>
-              </r-card>
+              </r-card> -->
               <r-card>
                    <r-table :data="data" />
               </r-card>
@@ -41,8 +41,9 @@ export default {
           [{'text':'走访时间'},{'text':'走访单位'},{'text':'操作'}]
         ],
         "body":[]
-      },
-      options: [{ key: 0, value: "未走访" }, { key: 1, value: "已走访" }],
+      }
+     // ,options: [{ key: 0, value: "未走访" }, { key: 1, value: "已走访" }] ,
+
     };
   }, 
   computed:{
@@ -59,7 +60,7 @@ export default {
                       this.condition.status = condition;
                   }
                   const identityId = Util.getIdentityId(this);
-                  const param = {"status":this.condition.status?this.condition.status:0,"classId":this.condition.class,"studentNos":this.condition.student_Nos,"startDateStr":this.condition.startDateStr,"endDateStr":this.condition.endDateStr,"pageNo":1,"pageSize":50} 
+                  const param = {"classId":this.condition.class,"studentNos":this.condition.student_Nos,"startDateStr":this.condition.startDateStr,"endDateStr":this.condition.endDateStr,"pageNo":1,"pageSize":50} //"status":this.condition.status?this.condition.status:0,
                   const surveys = await this.$http.post(`intern/student/intern/survey/list`,param);
                   const surveys_data = [];
                   _.each(surveys.body,(survey,index)=>{
